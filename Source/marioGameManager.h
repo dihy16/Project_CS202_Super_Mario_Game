db6b69209e727b3a973fc44fd22a721bf2860208
dinhhogiahuy290605@gmail.com <dinhhogiahuy290605@gmail.com>
2024-12-18 14:22:57 +0700

    change level1 class to level; replace direct members of Level with pointers (Mario, map)

--- Source/marioGameManager.h
+++ Source/marioGameManager.h
@@ -6,13 +6,39 @@
 #include "PhysicsEngine/Managers/RenderManager.h"
 #include "mario.h"  
 #include "GUI.h"
-//#include "level1.h"
+#include "enemy.h"
+#include "items.h"
+#include "map.h"
+
+class Mario;
+class Map;
+class Item;
+class Enemy;
+
+class Level
+{
+private:
+    Mario* mario;
+    Map* m;
+    std::vector<std::unique_ptr<Enemy>> enemies;
+    std::vector<std::unique_ptr<Item>> items;
+    sf::Image entitylayout;
+public:
+    bool display, finished;
+    Level(int level);
+    void handleKeyPress();
+    void start();
+    void end();
+    void execute();
+    void drawLevel();
+};
 
 class MarioGameManager : public GameManager {
 private:
     static MarioGameManager* instance;
     MenuManager* menuManager;   
     GUI* GUIManager;
+    Level* lv1;
     int marioLives = 3;
 	int score = 0;
 	int marioCoins = 0;
